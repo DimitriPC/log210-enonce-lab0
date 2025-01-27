85489682196f035f738074951b89659f76584629
DimitriPC <127147191+DimitriPC@users.noreply.github.com>
2025-01-26 23:19:34 -0500

    ajout tests redemarrerJeu

--- test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
+++ test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
@@ -1,9 +1,30 @@
-// Vous devez insérer les nouveaux tests ici
 import { assert } from 'console';
 import 'jest-extended';
+import { Joueur } from '../../src/core/joueur';
+import supertest from 'supertest'
+import app from '../../src/app';
+import { jeuRoutes } from '../../src/routes/jeuRouter';
 
-describe('redemarrerJeu.test.ts', () => {
-  it("devrait implémenter test", async () => {
-    throw new Error("Ce test n'a pas été défini")
+const request = supertest(app);
+
+let content = ""
+beforeAll(async () => {
+  const j1 = new Joueur('Joueur1');
+  const j2 = new Joueur('Joueur2');
+  await request.post('/api/v1/jeu/demarrerJeu').send({ nom: j1.nom});
+  await request.post('/api/v1/jeu/demarrerJeu').send({ nom: j2.nom});
+});
+
+describe('GET /api/v1/jeu/redemarrerJeu', () => {
+  it(`devrait répondre avec succès à appel pour redémarrerJeu`, async () => {
+    const response = await request.get('/api/v1/jeu/redemarrerJeu');
+    expect(response.status).toBe(200);
+    expect(response.type).toBe("application/json");
   });
+
+  it('devrait rendre 0 pour le nombre de Joueurs', async () => {
+      const joueursJSON = jeuRoutes.controleurJeu.joueurs;
+      const joueursArray = JSON.parse(joueursJSON);
+      expect(joueursArray.length).toBe(0);
+    });
 });
